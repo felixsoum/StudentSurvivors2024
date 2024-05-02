503c898e9ae42348b8a691521e4b7e6e5b5b7c21
Felix Soumpholphakdy <felix@felixsoum.com>
2024-05-02 10:49:40 -0400

    Ajout animation et reorientation du joueur

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,10 +7,12 @@ public class Player : MonoBehaviour
     [SerializeField] float scytheTimer = 2;
     float currentScytheTimer;
     Rigidbody2D rb;
+    Animator animator;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -37,5 +39,33 @@ public class Player : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
         rb.velocity = new Vector2(x, y) * movespeed;
+        animator.SetFloat("Speed", rb.velocity.magnitude);
+
+        if (x != 0)
+        {
+            int a;
+            if (x > 0)
+            {
+                a = 1;
+            }
+            else
+            {
+                a = -1;
+            }
+            transform.localScale = new Vector3(a, 1, 1);
+
+            int b = x > 0 ? 1 : -1;
+            transform.localScale = new Vector3(b, 1, 1);
+
+            transform.localScale = new Vector3(x > 0 ? 1 : -1, 1, 1);
+        }
     }
+
+
+    public int Foo()
+    {
+        return 5;
+    }
+
+    public int Foo2() => 5;
 }
